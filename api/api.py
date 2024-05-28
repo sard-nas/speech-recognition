deae3032c7d1cb36d650f4a1b9e2fed326c69a20
Alexandra Khusainova <khusainova.alexandra.20@gmail.com>
2024-05-28 17:03:47 +0500

    Fix formatting errors

--- api/api.py
+++ api/api.py
@@ -6,20 +6,18 @@ app = FastAPI()
 
 @app.get("/")
 async def read_root():
-	return {"message": "Распознавание речи"}
+    return {"message": "Распознавание речи"}
 
 
 @app.get("/record")
 async def record_voice():
-	recognizer = sr.Recognizer()
-	with sr.Microphone() as source:
-		audio = recognizer.listen(source, timeout=5.0)
-	try:
-		text = recognizer.recognize_google(audio, language="ru-RU")
-		return {"text": text}
-	except sr.UnknownValueError:
-		return {"error": "Речь не распознана"}
-	except BaseException as error:
-		return {"error": f"Неизвестная ошибка: error"}
-
-
+    recognizer = sr.Recognizer()
+    with sr.Microphone() as source:
+        audio = recognizer.listen(source, timeout=5.0)
+    try:
+        text = recognizer.recognize_google(audio, language="ru-RU")
+        return {"text": text}
+    except sr.UnknownValueError:
+        return {"error": "Речь не распознана"}
+    except BaseException as error:
+        return {"error": f"Неизвестная ошибка: {error}"}
--- api/main.py
+++ api/main.py
@@ -3,4 +3,5 @@ import os
 from api import app
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=int(os.getenv("PORT", "8080")), log_level="info")
+    uvicorn.run(app, host="0.0.0.0", port=int(os.getenv("PORT", "8080")),
+                log_level="info")
